30649ac21189d0cef1b3104f023d5db43138342c
tillias <ternovich@gmail.com>
2018-09-19 12:10:43 +0200

    Added CORS headers support

--- src/main/java/com/url/shortener/ShortenerController.java
+++ src/main/java/com/url/shortener/ShortenerController.java
@@ -1,13 +1,18 @@
 package com.url.shortener;
 
-import com.url.shortener.infrastructure.UrlNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ShortenerController {
+
+    // Needed for Heroku / Openshift deployments
+    private static final HttpHeaders CORS_HEADERS = create();
 
     private ShortenerService service;
 
@@ -17,30 +22,42 @@ public class ShortenerController {
     }
 
     @PostMapping("/shorten-url")
-    public Url shortenUrl(@RequestBody String source){
-        return service.shorten(source);
+    public ResponseEntity<Url> shortenUrl(@RequestBody String source) {
+        final Url url = service.shorten(source);
+        return new ResponseEntity<>(url, CORS_HEADERS, HttpStatus.OK);
     }
 
     @GetMapping("/url")
-    public List<Url> getUrls()
-    {
-        return service.getUrls();
+    public ResponseEntity<List<Url>> getUrls() {
+        final List<Url> urls = service.getUrls();
+        return new ResponseEntity<>(urls, CORS_HEADERS, HttpStatus.OK);
     }
 
     @GetMapping("/url/{id}")
-    public Url getUrl(@PathVariable String id){
+    public ResponseEntity<Url> getUrl(@PathVariable String id) {
         final Url url = service.getUrlByID(id);
 
-        if (url == null){
-            throw new UrlNotFoundException();
+        if (url == null) {
+            return ResponseEntity.notFound().build();
         }
 
-        return url;
+        return new ResponseEntity<>(url, CORS_HEADERS, HttpStatus.OK);
     }
 
     @GetMapping("/get-env/{envName}")
-    public String getEnv(@PathVariable String envName){
+    public String getEnv(@PathVariable String envName) {
         return System.getenv(envName);
+    }
+
+    private static HttpHeaders create() {
+        final HttpHeaders headers = new HttpHeaders();
+
+        headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Methods", "GET,PUT,POST");
+        headers.set("Access-Control-Allow-Headers", "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, " +
+                "X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control");
+
+        return headers;
     }
 
 }
