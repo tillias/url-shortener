30c013ce714166599f49e3ca5480b41c4f943203
tillias <ternovich@gmail.com>
2018-09-21 17:45:26 +0200

    Added native Spring CORS support

--- src/main/java/com/url/shortener/ShortenerController.java
+++ src/main/java/com/url/shortener/ShortenerController.java
@@ -2,19 +2,14 @@ package com.url.shortener;
 
 import com.url.shortener.infrastructure.UrlNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class ShortenerController {
-
-    // Needed for Heroku / Openshift deployments
-    private static final HttpHeaders CORS_HEADERS = create();
 
     private ShortenerService service;
 
@@ -23,39 +18,26 @@ public class ShortenerController {
         this.service = service;
     }
 
-    private static HttpHeaders create() {
-        final HttpHeaders headers = new HttpHeaders();
-
-        headers.set("Access-Control-Allow-Origin", "*");
-        headers.set("Access-Control-Allow-Methods", "GET,PUT,POST");
-        headers.set("Access-Control-Allow-Headers", "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, " +
-                "X-Requested-By, If-Modified-Since, X-File-Name, Cache-Control");
-
-        return headers;
-    }
-
     @PostMapping("/shorten-url")
-    public ResponseEntity<Url> shortenUrl(@RequestBody String source, @RequestParam(value = "custom-hash", required =
+    public Url shortenUrl(@RequestBody String source, @RequestParam(value = "custom-hash", required =
             false) String customHash) {
-        final Url url = service.shorten(source, customHash);
-        return new ResponseEntity<>(url, CORS_HEADERS, HttpStatus.OK);
+        return service.shorten(source, customHash);
     }
 
     @GetMapping("/url")
-    public ResponseEntity<List<Url>> getUrls() {
-        final List<Url> urls = service.getUrls();
-        return new ResponseEntity<>(urls, CORS_HEADERS, HttpStatus.OK);
+    public List<Url> getUrls() {
+        return service.getUrls();
     }
 
     @GetMapping("/url/{id}")
-    public ResponseEntity<Url> getUrl(@PathVariable String id) {
+    public Url getUrl(@PathVariable String id) {
         final Url url = service.getUrlByID(id);
 
         if (url == null) {
-            return ResponseEntity.notFound().build();
+            throw new UrlNotFoundException(String.format("Url id=%s not found", id));
         }
 
-        return new ResponseEntity<>(url, CORS_HEADERS, HttpStatus.OK);
+        return url;
     }
 
     @GetMapping("redirect/{id}")
@@ -68,15 +50,4 @@ public class ShortenerController {
 
         throw new UrlNotFoundException(String.format("Shortened url with id=%s is not found", id));
     }
-
-    @GetMapping("/get-env/{envName}")
-    public String getEnv(@PathVariable String envName) {
-        return System.getenv(envName);
-    }
-
-    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
-    public ResponseEntity handle() {
-        return new ResponseEntity(CORS_HEADERS, HttpStatus.OK);
-    }
-
 }
