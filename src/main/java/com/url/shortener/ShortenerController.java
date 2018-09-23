bd125383210bd3ad9de890fa078ca56f556c33d4
tillias <ternovich@gmail.com>
2018-09-23 19:49:43 +0200

    Added swagger documentation

--- src/main/java/com/url/shortener/ShortenerController.java
+++ src/main/java/com/url/shortener/ShortenerController.java
@@ -1,12 +1,17 @@
 package com.url.shortener;
 
 import com.url.shortener.infrastructure.UrlNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
+@Api(value = "shortener", description = "Operations for shortening urls")
 @CrossOrigin
 @RestController
 public class ShortenerController {
@@ -18,17 +23,29 @@ public class ShortenerController {
         this.service = service;
     }
 
+
+    @ApiOperation(value = "Performs url shortening action", response = Url.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully shortened url"),
+            @ApiResponse(code = 400, message = "Invalid URL has been provided"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 409, message = "You're trying to shorten url with custom hash, but different url " +
+                    "already uses this hash")
+    })
     @PostMapping("/shorten-url")
     public Url shortenUrl(@RequestBody String source, @RequestParam(value = "custom-hash", required =
             false) String customHash) {
         return service.shorten(source, customHash);
     }
 
+    @ApiOperation(value = "Gets list of all urls. ApiResponses will be described in version 2.0")
     @GetMapping("/url")
     public List<Url> getUrls() {
         return service.getUrls();
     }
 
+    @ApiOperation(value = "Gets url using its id. ApiResponses will be described in version 2.0")
     @GetMapping("/url/{id}")
     public Url getUrl(@PathVariable String id) {
         final Url url = service.getUrlByID(id);
@@ -40,6 +57,8 @@ public class ShortenerController {
         return url;
     }
 
+    @ApiOperation(value = "Attempts to find url by it's hash and if found redirects. ApiResponses will be described " +
+            "in version 2.0")
     @GetMapping("redirect/{id}")
     public RedirectView redirect(@PathVariable String id) {
         final Url url = service.getUrlByID(id);
@@ -51,6 +70,7 @@ public class ShortenerController {
         throw new UrlNotFoundException(String.format("Shortened url with id=%s is not found", id));
     }
 
+    @ApiOperation(value = "Deletes url. ApiResponses will be described in version 2.0")
     @DeleteMapping("url/{id}")
     public void deleteUrl(@PathVariable String id) {
         service.deleteUrl(id);
